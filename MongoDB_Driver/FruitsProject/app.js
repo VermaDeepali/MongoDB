f7b91ac35fccd17614acde1d8383c2907e75cc48
Deepali Verma <deepaliv679@gmail.com>
2020-12-18 14:48:24 +0530

    first commit

---
+++ MongoDB_Driver/FruitsProject/app.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection url
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitsDB';
+
+//Create a new MongoClient
+const client = new MongoClient(url,{ useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+
+client.connect(function(err){
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  client.close();
+
+});
